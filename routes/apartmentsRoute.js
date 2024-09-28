db2025f7747d5dc3be67c0da2075abf177b71ab7
ose sunday <ose.sunday@gmail.com>
2024-09-28 19:15:54 -0400

    apart

--- routes/apartmentsRoute.js
+++ routes/apartmentsRoute.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const router = express.Router();
 const apartmentController= require('./../controllers/apartmentController')
+const authController= require('./../controllers/authController.js')
 
 
 const {createApartment, getAllApartments, getApartmentById, updateApartment, deleteApartment} = apartmentController
+const {protect} = authController
+
 
 
 router
     .route(`/`)
-    .post(createApartment)
-    .get(getAllApartments)
+    .post(protect, createApartment)
+    .get(protect, getAllApartments)
 
+    
 router
     .route(`/:id`)
-    .get(getApartmentById)
-    .patch(updateApartment)
-    .delete(deleteApartment);
+    .get(protect, getApartmentById)
+    .patch(protect, authController.restrictTo('admin'), updateApartment)
+    .delete(protect, authController.restrictTo('admin'), deleteApartment);
+
 
 module.exports = router;
